1ed82a0715e856f201b11e00a962156b92eef0cc
1Kopal <kopal.agarwal@students.iiit.ac.in>
2018-11-04 00:07:17 +0530

    Ignore commit

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from flask import url_for, redirect, request, make_response,flash
 from flask import session
 from app.models import Login
 from app import app, db
+import pandas as pd
 
 app.secret_key = 'MKhJHJH798798kjhkjhkjGHh'
 
@@ -12,16 +13,30 @@ app.secret_key = 'MKhJHJH798798kjhkjhkjGHh'
 def index():
 	return render_template('login.html')
 
+def CreateMessFile(messFileName):
+	open('app/UserFiles/'+messFileName, "wb").close()
+	# datelist = pd.date_range(end=pd.datetime.today().date(), periods=180).tolist()
+	# l = [0] * 16
+	# for i in range(181):
+	# 	newDateEntry={'date':datelist[i],'mess_meal':l,'total':0}
+	# 	with open("UserFiles/"+messFileName, "ab") as f:
+	# 		pickle.dump(newDateEntry,f)
+			
+	
+
+
 @app.route('/register')
 def register():
 	return render_template('login.html')
 
 @app.route('/registerNext', methods = ['GET','POST'])
 def registerNext():
-	user = Login(firstname=request.form["fname"], lastname=request.form["lname"], email=request.form["email"], password=request.form['loginpassword'], messFile=request.form["email"]+".txt")
+	messFileName=request.form["email"]+".txt"
+	user = Login(firstname=request.form["fname"], lastname=request.form["lname"], email=request.form["email"], password=request.form['loginpassword'], messFile=messFileName)
 	db.session.add(user)
 	db.session.commit()
 
+	CreateMessFile(messFileName)
 	flash('Your account has been created! You are now able to log in', 'success')
 	return "Register Successful for: %s" % user.firstname
 
