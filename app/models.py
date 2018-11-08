94ff2a3fba6ccfa5ecf27b1bbc19967e38c5ef15
anand13sethi <anandsethi4@gmail.com>
2018-11-08 13:13:47 +0530

    Tried to add JSON to DB, debugging left.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from app import db
 
+
 class Login(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(20), nullable=False)
@@ -8,10 +9,11 @@ class Login(db.Model):
     email = db.Column(db.String(30), unique=True, nullable=False)
     rollNo = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
-    messFile=db.Column(db.String(35), nullable=True)
+    json = db.Column(db.String(35), nullable=True)
 
     def __repr__(self):
         return "Login('{self.email}')"
+
 
 db.create_all()
 
--- app/views.py
+++ app/views.py
@@ -6,8 +6,11 @@ from app.models import Login
 from app import app, db
 import pandas as pd
 from passlib.hash import sha256_crypt
+import datetime
+import json
 
 app.secret_key = 'MKhJHJH798798kjhkjhkjGHh'
+
 
 @app.route('/index')
 @app.route('/')
@@ -24,15 +27,27 @@ def index():
 	# 		pickle.dump(newDateEntry,f)
 
 
+def init_json():
+	date_list = [datetime.datetime(2019, 6, 30) - datetime.timedelta(days=x) for x in range(540)]
+	for item in range(len(date_list)):
+		date_list[item] = date_list[item].date()
+	dic = {}
+	for i in date_list:
+		dic[i] = [[[0 for _ in range(4)] for _ in range(4)], 0]
+	json_data = json.dumps(dic)
+	return json_data
+
+
 @app.route('/register')
 def register():
 	return render_template('login.html')
 
-@app.route('/registerNext', methods = ['GET','POST'])
+
+@app.route('/registerNext', methods=['GET', 'POST'])
 def registerNext():
 	try:
-		messFileName=request.form["email"]+".txt"
-		user = Login(firstname=request.form["fname"], lastname=request.form["lname"], email=request.form["email"],rollNo=request.form["rollNo"],password=request.form['loginpassword'], messFile=messFileName)
+		# json_name = request.form["rollNo"]
+		user = Login(firstname=request.form["fname"], lastname=request.form["lname"], email=request.form["email"], rollNo=request.form["rollNo"], password=request.form['loginpassword'], json=init_json())
 		db.session.add(user)
 		db.session.commit()
 	except:
@@ -46,6 +61,7 @@ def registerNext():
 def login():
 	return render_template('login.html')
 
+
 @app.route('/loginNext',methods=['GET','POST'])
 def loginNext():
 
@@ -53,11 +69,12 @@ def loginNext():
 		rollNo = request.form['LrollNo']
 		password = request.form['Lloginpassword']
 		
-		user  = Login.query.filter(and_(Login.rollNo == rollNo, Login.password == password)).first()
+		user = Login.query.filter(and_(Login.rollNo == rollNo, Login.password == password)).first()
 		if user:
 			flash('Login successful', 'success')
 			return "Login Successful for: %s" % user.firstname
 		return "Password Error"
+
 
 @app.route('/home.html')
 @app.route('/home')
